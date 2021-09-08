325891bdf362310ccc2b39fd16314f9f85cfb02f
ZheNing Hu <adlternative@gmail.com>
2021-09-08 16:36:05 +0800

    lab2a: 2A.sh better show
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- src/raft/2A.sh
+++ src/raft/2A.sh
@@ -1,8 +1,8 @@
-for ((i=0;i<100;i++))
-do
-go test -run 2A -race  -gcflags=all="-N -l"  1>a.err
-if test $? -ne 0
-then
-	exit 0
-fi
+for ((i = 0; i < 50; i++)); do
+	go test -run 2A -race -gcflags=all="-N -l" 1>a.err
+	if test $? -ne 0; then
+		exit 1
+	else
+		echo "batch $i ok"
+	fi
 done
