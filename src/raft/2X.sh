e7d8e24d0f680bb1b424f5136728f3e4df422996
ZheNing Hu <adlternative@gmail.com>
2021-09-13 19:10:27 +0800

    lab2B: 2X.sh update
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- src/raft/2X.sh
+++ src/raft/2X.sh
@@ -1,5 +1,13 @@
+##
+## sh ./2X.sh [A|B|C|D] <test>
+##
 for ((i = 0; i < 50; i++)); do
-	go test -run 2$1 -race -gcflags=all="-N -l" 1>$1.err
+	if [ "$2" != "" ]; then
+		go test -run 2$1 -race -test.run $2 -gcflags=all="-N -l" 1>$1.err
+	else
+		go test -run 2$1 -race -gcflags=all="-N -l" 1>$1.err
+	fi
+
 	if test $? -ne 0; then
 		echo "batch $i failed"
 		exit 1
