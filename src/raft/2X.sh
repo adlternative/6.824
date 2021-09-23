36aee766a410b6a574613cd2e367d0bb0c99ad5e
ZheNing Hu <adlternative@gmail.com>
2021-09-23 20:06:43 +0800

    lab2D: change 2X.sh
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- src/raft/2X.sh
+++ src/raft/2X.sh
@@ -3,16 +3,17 @@
 ##  E.G. ./2X.sh B TestFailNoAgree2B 3
 ##  E.G. ./2X.sh B ALL 50
 ##
+go test -c -race -gcflags=all="-N -l"
 for ((i = 0; i < $3; i++)); do
-	if [ "$2" != "ALL" ]; then
-		go test -run 2$1 -race -test.run $2 -gcflags=all="-N -l" 1>$1.err
+	if [ "$2" != "." ]; then
+		./raft.test -test.run $2  1>$1.err 2>&1
 	else
-		go test -run 2$1 -race -gcflags=all="-N -l" 1>$1.err
+		./raft.test 1>$1.err 2>&1
 	fi
 
 	if test $? -ne 0; then
 		echo "batch $i failed"
-		./debug >$1-format.err
+		# ./debug >$1-format.err
 		exit 1
 	else
 		echo "batch $i ok"
